f87ed4f4150e4285cb67350258db08a113dc47ed
ankita26294 <105727081+ankita26294@users.noreply.github.com>
2023-08-28 10:55:49 +0530

    Add files via upload

---
+++ Q.11 sql.sql
@@ -0,0 +1,13 @@
+create database socialbyte;
+use socialbyte;
+
+CREATE TABLE `Student Table` ( `ID` VARCHAR(20) NOT NULL PRIMARY KEY ,`Name` VARCHAR(20),  
+`Age` INT NOT NULL ,`Address` VARCHAR(25) NOT NULL);
+
+insert into `Student Table` values
+('1','ankita','28','pune'),
+('2','Anvi','64','mumbai'),
+('3','chetan','56','pune'),
+('4','kum','34','xyz'),
+('6','gungun','50','prq');  
+
